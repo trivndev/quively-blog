07ccbf131e0f8ad2db3699dabae6b048b1d9cea8
Claire <nicolas100107@gmail.com>
2025-06-19 19:15:32 +0700

    fix ui

--- resources/views/components/article.blade.php
+++ resources/views/components/article.blade.php
@@ -24,15 +24,15 @@
 
             </address>
         </div>
-        <div id="content" class="space-y-12 ">
-            <h4 class="text-3xl font-extrabold text-gray-900 lg:text-4xl dark:text-white w-full leading-none">
+        <div id="content" class="space-y-12 max-w-full">
+            <h4 class="text-3xl font-extrabold text-gray-900 lg:text-4xl dark:text-white break-words">
                 {{ $post->title }}</h4>
             <a href="/blog?category={{ $post->category->slug }}"
                title="Go to {{ $post->category->name }} category"
                class="{{ $post->category->color }} dark:bg-primary-200 dark:text-primary-800 hover:bg-primary-200 dark:hover:bg-primary-300 block w-fit items-center rounded px-2.5 py-2 text-xs font-medium text-gray-800 transition-all duration-300 ease-in-out hover:text-black no-underline">
                 {{ $post->category->name }}
             </a>
-            <div class="text-justify">
+            <div class="text-justify break-words">
                 {!! $post->blog_content !!}
             </div>
         </div>
