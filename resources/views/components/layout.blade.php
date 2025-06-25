d554480b30d96f17c1c81b568bcdb5745501e122
Claire <nicolas100107@gmail.com>
2025-06-25 21:30:56 +0700

    Adding scroll smooth

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,34 +1,35 @@
 <!doctype html>
-<html lang="en">
+<html lang="en" class="scroll-smooth">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description"
-          content="Quively is a platform for writers to share their work with the world.">
+    <meta name="description" content="Quively is a platform for writers to share their work with the world.">
     <title>{{ $title }}</title>
     <link rel="icon" href="/quively.svg">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
-            href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
-            rel="stylesheet">
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+        rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.8/dist/cdn.min.js"></script>
-   @stack('filepond-style')
+    @stack('filepond-style')
     @stack('quil-style')
 </head>
+
 <body class="h-full bg-slate-100">
-<x-navbar/>
+    <x-navbar />
 
-<main>
-    {{ $slot }}
-</main>
+    <main>
+        {{ $slot }}
+    </main>
 
-<script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
-@stack('filepond-script')
-@stack('quil-js')
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+    @stack('filepond-script')
+    @stack('quil-js')
 </body>
+
 </html>
